e4fcbbc5ba16d5dd156a56804d2ad4e3884dabbf
Unknown <allocompte@hotmail.fr>
2020-02-21 21:10:59 +0100

    Add User to remote sequelize instance

--- backend/models/index.ts
+++ backend/models/index.ts
@@ -8,6 +8,7 @@ function initialize() {
       // tslint:disable-next-line:max-line-length
       return new Sequelize('postgres://nhoaxhodqgkzkz:e845d362d6ba346e90c3f2abf3f28f2df877d98d530255f3297ab85e9009cb1c@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/d947ik3d5ejhrd', {
         ssl: true,
+        models: [User]
       });
     }
 
