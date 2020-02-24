a7277573bb62aae81849a9811f0299957ab412e9
Unknown <allocompte@hotmail.fr>
2020-02-24 16:39:50 +0100

    Remove SSR code

--- server.ts
+++ server.ts
@@ -18,27 +18,14 @@ db.initialize().then(() => {
   const PORT = process.env.PORT || 4200;
   const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
-  const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
-
-  app.engine('html', ngExpressEngine({
-    bootstrap: AppServerModuleNgFactory,
-    providers: [
-      provideModuleMap(LAZY_MODULE_MAP)
-    ]
-  }));
-
   app.set('view engine', 'html');
   app.set('views', DIST_FOLDER);
 
   routes.setup(app);
 
-  app.get('*.*', express.static(DIST_FOLDER, {
+  app.get('*.*|/', express.static(DIST_FOLDER, {
     maxAge: '1y'
   }));
-
-  app.get('*', (req, res) => {
-    res.render('index', {req});
-  });
 
   app.listen(PORT, () => {
     console.log(`Node Express server listening on http://localhost:${PORT}`);
