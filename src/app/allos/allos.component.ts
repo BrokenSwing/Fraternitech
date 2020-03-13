7d2e73e58f2f862a0101ca9aecbed0d7d16801b3
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-03-13 17:13:24 +0100

    Add allos data

--- src/app/allos/allos.component.ts
+++ src/app/allos/allos.component.ts
@@ -8,32 +8,83 @@ import { Component, OnInit } from '@angular/core';
 export class AllosComponent implements OnInit {
 
   private static ALL_ALLOS = [{
-    name: 'Allô ménage',
-    description: 'Notre équipe vient faire le ménage chez vous',
+    name: 'Aspir\'allô',
+    description: 'Nettoyer, balayer, c\'est notre métier',
   }, {
-    name: 'Allô bière',
-    description: 'Notre équipe vous ramène une bière',
+    name: 'Chich\'allô',
+    description: 'Le allô des beubeu',
   }, {
-    name: 'Allô massage',
-    description: 'Notre équipe vient vous masser',
+    name: 'All\'orizontal',
+    description: 'Non c\'est pas du cul c\'est des massages désolé',
   }, {
-    name: 'Allô kebab',
-    description: 'Notre équipe vous commande et livre un kebab',
+    name: 'Apér\'allô',
+    description: 'On amène des bières et des gâteaux apéro',
   }, {
-    name: 'Allô tacos',
-    description: 'Notre équipe vous commande et vous livre un tacos',
+    name: 'Matéri\'allô',
+    description: 'On vous rase le crâne dignement (ou toute autre coupe)',
   }, {
-    name: 'Allô crêpe',
-    description: 'Notre équipe vous prépare une crêpe et vous la livre',
+    name: 'Allô Stifl\'hug',
+    description: 'On envoie Stifler te faire des câlins',
+  }, {
+    name: 'Coucou, tu veux voir ma banane ?',
+    description: 'On vient avec un objet surprise dans la banane'
+  }, {
+    name: 'All\'autobus',
+    description: 'On vous ramène chez vous',
+    note: 'Seulement le jeudi et le week-end, pendant toute la nuit'
+  }, {
+    name: 'NBAllô',
+    description: 'On vient tester vos talents de basketteur',
+    note: '1 point par tire réussi, 1 fois par jour seulement'
+  }, {
+    name: 'Catch\'allô',
+    description: 'Un catchou de Klité',
+    note: 'Si les listeux boivent 13 verres ou plus, chaque autre joueur gagne 3 points'
+  }, {
+    name: 'Boire A LL\'envers',
+    description: 'Poirier binouze des familles',
+    note: 'Objectif : tenir 15 secondes pour gagner 4 points'
+  }, {
+    name: 'Une balle, une table, des verres',
+    description: 'On se ramène avec notre super table décorée main',
+    note: 'Si vous sortez vainqueurs, vous gagnez 3 points. A réserver en avance'
+  }, {
+    name: 'Coron\'allô',
+    description: 'Des conseils pour survivre du coronavirus pendant ces campagnes'
+  }, {
+    name: 'AllôSaMèRe',
+    description: 'Un peu d\'ASMR pour vous détendre'
+  }, {
+    name: 'All\'oroscope',
+    description: 'Nos voyants nous ont déjà prévenu que tu nous appelle pour débarasser ton verre'
+  }, {
+    name: 'AHAHA\'llô',
+    description: 'Tata Francine a intérêt à retenir son chicot'
+  }, {
+    name: 'Que cALLÔr',
+    description: 'Venez tester notre téléphone rose'
+  }, {
+    name: 'SAH\'llô',
+    description: '... Oo'
+  }, {
+    name: 'Allô ptit dej',
+    description: 'On t\'apporte le petit déjeuner directement chez toi'
+  }, {
+    name: 'Allô popcorn',
+    description: 'Qu\'il soit salé ou sucré, on a mit tout l\'air de nos poumons pour le souffler ce maïs'
+  }, {
+    name: 'Allô restauration',
+    description: 'On vous libre à domicile',
+    note: 'Paiement par lyf ou lydia'
   }];
 
-  allos: Array<Array<{name: string, description: string}>> = [];
+  allos: Array<Array<{name: string, description: string, note?: string}>> = [];
 
   constructor() {
-    this.allos = Array.from(Array(AllosComponent.ALL_ALLOS.length / 3).keys()).map((i) => {
-      const first = AllosComponent.ALL_ALLOS[i];
-      const second = AllosComponent.ALL_ALLOS[i + 1];
-      const third = AllosComponent.ALL_ALLOS[i + 2];
+    this.allos = Array.from(Array(Math.ceil(AllosComponent.ALL_ALLOS.length / 3)).keys()).map((i) => {
+      const first = AllosComponent.ALL_ALLOS[i * 3];
+      const second = AllosComponent.ALL_ALLOS[i * 3 + 1];
+      const third = AllosComponent.ALL_ALLOS[i * 3 + 2];
       const result = [first];
       if (second !== undefined) {
         result.push(second);
