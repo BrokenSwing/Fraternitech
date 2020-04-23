34bf1bed4c5518b846f60f2b7dca1a614cf022a6
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-04-23 12:52:30 +0200

    Change end date for baby pics

--- backend/timings/index.ts
+++ backend/timings/index.ts
@@ -2,4 +2,4 @@ export const SHOW_AT = 10;
 export const CLOSE_AT = 0;
 export const UPDATE_SCORES_AT = 0;
 export const UNIT = 'h';
-export const TIME_TO_RESPOND = 48;
+export const TIME_TO_RESPOND = 24 + 5; // 22/04 11h - 23/400 16h
